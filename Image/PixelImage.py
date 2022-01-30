3bc125c03e847822c16f0be7f5ee71a7959d4078
David Marx <david.marx84@gmail.com>
2022-01-29 18:09:59 -0800

    explicit imports for pytti.named_rearrange

--- Image/PixelImage.py
+++ Image/PixelImage.py
@@ -1,5 +1,5 @@
 #from pytti import *
-from pytti import DEVICE
+from pytti import DEVICE, named_rearrange
 from pytti.Image import DifferentiableImage
 from pytti.LossAug import HSVLoss
 from pytti.ImageGuide import DirectImageGuide
