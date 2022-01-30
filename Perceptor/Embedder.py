320e7fa12f2c5db2051603615c80516fc947357d
David Marx <david.marx84@gmail.com>
2022-01-29 18:20:47 -0800

    explicit imports for pytti.normalize

--- Perceptor/Embedder.py
+++ Perceptor/Embedder.py
@@ -1,6 +1,6 @@
 #from pytti import *
 import pytti
-from pytti import DEVICE, format_input, cat_with_pad, format_module
+from pytti import DEVICE, format_input, cat_with_pad, format_module, normalize
 
 import torch
 from torch import nn
