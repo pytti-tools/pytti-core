a2add7c0d878192fd63fa4e06826b337fc9976c7
David Marx <david.marx84@gmail.com>
2022-02-09 17:32:12 -0800

    re-added submodule dependencies under 'vendor' dir

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages, find_namespace_packages
+import logging
+
+p0 = find_packages(where='src')
+#print(p0)
+#logging.critical(p0)
+p1 = find_namespace_packages(where='vendor')
+#print(p1)
+#logging.critical(p1)
+
+setup(
+    #packages= p0 + p1,
+    packages=p0,
+    package_dir={
+        "": "src",
+        #"pyttitools-adabins": "vendor",
+        #"pyttitools-gma": "vendor",
+        },
+    install_requires=['pyttitools-adabins', 'pyttitools-gma']
+)
---
+++ vendor/README.md
@@ -0,0 +1,1 @@
+Vendored code goes here.
