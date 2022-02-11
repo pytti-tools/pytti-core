5c278878fe5f68040c9d0acb480f02da4b6e6f0a
David Marx <david.marx84@gmail.com>
2022-02-11 15:16:32 -0800

    added hydra plugin

---
+++ src/pytti/hydra_plugins/searchpath_append_cwd.py
@@ -0,0 +1,19 @@
+from hydra.core.config_search_path import ConfigSearchPath
+from hydra.plugins.search_path_plugin import SearchPathPlugin
+
+from loguru import logger
+
+# https://hydra.cc/docs/advanced/search_path/#
+# https://github.com/facebookresearch/hydra/issues/763
+
+class PyttiLocalConfigSearchPathPlugin(SearchPathPlugin):
+
+    def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
+
+        local_path = f"{os.getcwd()}/config/"
+        # If path doesn't exist, we should create it and copy configs to it
+        logger.debug(local_path)
+
+        search_path.append(
+            provider="pytti_hydra_pathplugin", path="file://${local_path}/"
+        )
