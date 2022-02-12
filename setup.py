14b1150e5e707798639190206878f97953cb13ed
David Marx <david.marx84@gmail.com>
2022-02-11 16:15:01 -0800

    added hydra_plugins to setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ import logging
 p0 = find_packages(where='src')
 #print(p0)
 #logging.critical(p0)
-p1 = find_namespace_packages(where='vendor')
+p1 = find_namespace_packages(where='vendor.*')
 #print(p1)
 #logging.critical(p1)
+p2 = find_namespace_packages(where='hydra_plugins.*')
+logging.critical(p2)
 
 setup(
     #packages= p0 + p1,
-    packages=p0,
+    packages=p0+p2,
     package_dir={
         "": "src",
         #"pyttitools-adabins": "vendor",
