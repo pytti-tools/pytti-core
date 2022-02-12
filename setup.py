acb0cd346d4f4682949584385b0462a06c4bebec
David Marx <david.marx84@gmail.com>
2022-02-11 16:25:37 -0800

    modified package search for hydra plugins

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import logging
 p0 = find_packages(where='src')
 #print(p0)
 #logging.critical(p0)
-p1 = find_namespace_packages(where='vendor.*')
+#p1 = find_namespace_packages(where='vendor.*')
 #print(p1)
 #logging.critical(p1)
-p2 = find_namespace_packages(where='hydra_plugins.*')
-logging.critical(p2)
+p2 = find_namespace_packages(where='hydra_plugins')
+#logging.critical(p2)
 
 setup(
     #packages= p0 + p1,
