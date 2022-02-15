f463437db2db3ab058d763561375be96c640a399
David Marx <david.marx84@gmail.com>
2022-02-15 12:26:02 -0800

    added a note

--- src/pytti/tensor_tools.py
+++ src/pytti/tensor_tools.py
@@ -7,7 +7,7 @@ normalize = transforms.Normalize(
     mean=[0.48145466, 0.4578275, 0.40821073], std=[0.26862954, 0.26130258, 0.27577711]
 )
 
-
+# Stuff like this could probably be replaced with einops
 def named_rearrange(tensor, axes, new_positions) -> torch.tensor:
     """
     Permute and unsqueeze tensor to match target dimensional arrangement
