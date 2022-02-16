b5c23edb348b5a2aadd472bd851d8dd01f2a7677
David Marx <david.marx84@gmail.com>
2022-02-15 20:08:14 -0800

    added test skeletons

---
+++ tests/test_prompt_dsl.py
@@ -0,0 +1,25 @@
+from Pytti.Perceptor.Prompt import Prompt, parse_prompt
+
+
+def test_prompt_parsing():
+    pass
+
+
+def test_weight_parsing():
+    pass
+
+
+def test_stop_weight_parsing():
+    pass
+
+
+def test_stop_weight_parsing():
+    pass
+
+
+def test_semantic_mask_parsing():
+    pass
+
+
+def test_direct_image_mask_parsing():
+    pass
