38a18a039e77fa889b222cc9bdb00c22af0cabf7
David Marx <david.marx84@gmail.com>
2022-05-02 15:04:39 -0700

    added test for mlf models

--- tests/test_mmc_loaders.py
+++ tests/test_mmc_loaders.py
@@ -10,7 +10,7 @@ CONFIG_BASE_PATH = "config"
 CONFIG_DEFAULTS = "default.yaml"
 
 
-cfg_yaml0 = """# @package _global_
+cfg_yaml_openai = """# @package _global_
 scenes: a photograph of an apple
 use_mmc: true
 mmc_models:
@@ -30,9 +30,35 @@ def test_mmc_openai_models():
             config_name=CONFIG_DEFAULTS,
             overrides=[f"conf=_empty"],
         )
-        cfg_mmc = OmegaConf.create(cfg_yaml0)
+        cfg_mmc = OmegaConf.create(cfg_yaml_openai)
 
         with open_dict(cfg_base) as cfg:
-            # cfg.update(cfg_mmc)
+            cfg = OmegaConf.merge(cfg_base, cfg_mmc)
+        render_frames(cfg)
+
+
+cfg_yaml_mlf = """# @package _global_
+scenes: a photograph of an apple
+use_mmc: true
+mmc_models:
+- architecture: clip
+  publisher: mlfoundations
+  id: RN50--yfcc15m
+- architecture: clip
+  publisher: mlfoundations
+  id: ViT-B-32--laion400m_avg
+"""
+
+
+def test_mmc_mlf_models():
+
+    with initialize(config_path=CONFIG_BASE_PATH):
+        cfg_base = compose(
+            config_name=CONFIG_DEFAULTS,
+            overrides=[f"conf=_empty"],
+        )
+        cfg_mmc = OmegaConf.create(cfg_yaml_mlf)
+
+        with open_dict(cfg_base) as cfg:
             cfg = OmegaConf.merge(cfg_base, cfg_mmc)
         render_frames(cfg)
